0b3989db6f65b4d99fefa124ba0296c8b11c887f
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2021-10-25 13:57:06 +0200

    pr comments

--- ebl_ai/check_installation.py
+++ ebl_ai/check_installation.py
@@ -1,27 +1,22 @@
-# pyre-ignore-all-errors
 # Check Pytorch installation
 import torch
 import torchvision
+import mmcv
+from mmcv.ops import get_compiling_cuda_version, get_compiler_version
+import mmdet
+import mmocr
 
 print("Torch version: ", torch.__version__)
 print("Is Cude Available: ", torch.cuda.is_available())
-print("Torchvision version: ", torchvision.__version__)
-
-
-# Check mmcv installation
-import mmcv  # noqa: E402
+print("Torchvision version: ", torchvision.__version__)  # pyre-ignore[16]
 
 print("MMCV version: ", mmcv.__version__)
-from mmcv.ops import get_compiling_cuda_version, get_compiler_version  # noqa: E402
+
 
 print("MMCV Cuda compiling version: ", get_compiling_cuda_version())
 print("Compiler version: ", get_compiler_version())
 
-# Check MMDetection installation
-import mmdet  # noqa: E402
 
 print("MMDET version: ", mmdet.__version__)
-# Check mmocr installation
-import mmocr  # noqa: E402
 
 print("MMOCR version: ", mmocr.__version__)
