860564e9a181f1a5f17da9c8c6617bfb3a0ae0c4
yCobanoglu <yunus.cobanoglu@campus.lmu.de>
2021-11-16 00:36:57 +0100

    rename new checkpoint

--- ebl_ai/app.py
+++ ebl_ai/app.py
@@ -14,6 +14,6 @@ def create_app(model: Model):
 def get_app():
     model = Model(
         configFile="model/fcenet_no_dcvn.py",
-        checkpoint="model/checkpoint.pth",
+        checkpoint="model/checkpoint1.pth",
     )
     return create_app(model)
